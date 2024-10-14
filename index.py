e5f926181d937e2abf87811c8b3ebf790c8a9834
Naomi Takagaki <natakagaki@up.edu.ph>
2024-10-14 09:14:03 +0800

    update

--- index.py
+++ index.py
@@ -20,6 +20,8 @@ CONTENT_STYLE = {
     "margin-top" : "1em",  
 }
 
+server = app.server
+
 app.layout = html.Div(
     [
         html.Meta(
@@ -134,6 +136,9 @@ def displaypage(pathname, sessionlogout, user_id, accesstype, search):
 
     return [returnlayout, sessionlogout]
 
+app = dash.Dash(__name__)
+server = app.server
+
+
 if __name__ == '__main__':
-    webbrowser.open('http://127.0.0.1:8050/', new = 0, autoraise = True)
-    app.run_server(debug = False)
+    app.run_server(debug=True)
