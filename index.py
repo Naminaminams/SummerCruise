7ca6a53d77ebe24875192854cfe501ba5400884e
Naomi Takagaki <natakagaki@up.edu.ph>
2025-01-13 15:06:32 +0800

    push

--- index.py
+++ index.py
@@ -116,8 +116,7 @@ def displaypage(pathname, sessionlogout, user_id, accesstype, search):
     return [returnlayout, sessionlogout]
  
 
-if __name__ == '__main__':
-    webbrowser.open('http://127.0.0.1:8050/')
+if __name__ == '__main__': 
     app.run_server(debug=True)
 
 
